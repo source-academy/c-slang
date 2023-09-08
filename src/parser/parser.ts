3b8f31e6403aacf85f58f2e802a2d3ca7e39319b
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2023-09-08 17:59:03 +0800

    Set up wasm AST nodes

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -1,4 +1,4 @@
-import grammar from "grammar/main.pegjs";
+import grammar from "./main.pegjs";
 import * as peggy from "peggy";
 const parser = peggy.generate(grammar as string, {
   allowedStartRules: ["program"],
--- src/wasm-ast/wasm-nodes.ts
+++ src/wasm-ast/wasm-nodes.ts
@@ -1,4 +1,81 @@
 /**
  * This file contains all the typescript definitions for the nodes of the wasm AST.
  */
+type WasmType = "i32" | "i64" | "f32" | "f64";
 
+export interface WasmVariable {
+  name: string; // not technically needed for wasm, but useful
+  variableType: WasmType;
+}
+
+export interface WasmLocalVariable extends WasmVariable {
+  type: "LocalVariable";
+}
+
+export interface WasmGlobalVariable extends WasmVariable {
+  type: "GlobalVariable";
+}
+
+export interface WasmConst {
+  type: "Const";
+  variableType: WasmType;
+  value: number;
+}
+
+export interface WasmModule {
+  type: "Module";
+  globals: WasmVariable[];
+  functions: WasmFunction[];
+}
+
+export interface WasmFunction {
+  type: "Function";
+  params: WasmVariable[];
+  locals: WasmVariable[];
+  body: (WasmStatement | WasmExpression)[];
+  return: WasmType;
+}
+
+type WasmStatement = WasmGlobalSet | WasmLocalSet;
+
+// TODO: figure out if this necessary
+type WasmExpression =
+  | WasmFunctionCall
+  | WasmConst
+  | WasmLocalGet
+  | WasmGlobalGet;
+
+export interface WasmFunctionCall {
+  type: "FunctionCall";
+  name: string;
+  args: WasmExpression[];
+}
+
+// A procedure is a function with no return value
+export interface WasmProcedureCall {
+  type: "ProcedureCall";
+  name: string;
+  args: WasmExpression[];
+}
+
+export interface WasmGlobalSet {
+  type: "GlobalSet";
+  name: string;
+  value: WasmExpression;
+}
+
+export interface WasmLocalSet {
+  type: "LocalSet";
+  name: string;
+  value: WasmExpression;
+}
+
+export interface WasmLocalGet {
+  type: "LocalGet";
+  name: string;
+}
+
+export interface WasmGlobalGet {
+  type: "GlobalGet";
+  name: string;
+}
