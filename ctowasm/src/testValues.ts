060e1a0e585c12332df55e204df5959ea7fd54f6
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2023-10-20 16:53:01 +0800

    Remove redundant file

--- ctowasm/src/testValues.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Add import of a log function and the call of logs for each variable in main - part of testing process.
- */
-function addDebugStatements(watStr: string) {
-  const indexToAddLogImport = watStr.indexOf("(memory");
-  const indexToAddLogStatements = watStr.indexOf(")\n\t(start $main)")
-  
-}
