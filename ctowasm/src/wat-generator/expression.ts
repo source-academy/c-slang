edd9711b9c17605071676d7fda7701558ae173e7
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2023-12-13 15:31:33 +0800

    Refactor wat generator

---
+++ ctowasm/src/wat-generator/expression.ts
@@ -0,0 +1,85 @@
+import { WasmExpression, WasmConst } from "~src/wasm-ast/core";
+import { WasmFunctionCall } from "~src/wasm-ast/functions";
+import { WasmMemoryLoad } from "~src/wasm-ast/memory";
+import {
+  WasmArithmeticExpression,
+  WasmComparisonExpression,
+  WasmBooleanExpression,
+  WasmAndExpression,
+  WasmOrExpression,
+} from "~src/wasm-ast/operations";
+import { WasmLocalGet, WasmGlobalGet } from "~src/wasm-ast/variables";
+import { generateStatementStr } from "~src/wat-generator/statement";
+import {
+  generateStatementsList,
+  getPreStatementsStr,
+  getBinaryInstruction,
+  getWasmMemoryLoadInstruction,
+} from "~src/wat-generator/util";
+
+/**
+ * Given a wat Expression node, generates the string version of that expression, with brackets.
+ */
+export function generateExprStr(expr: WasmExpression): string {
+  if (expr.type === "FunctionCall") {
+    const e = expr as WasmFunctionCall;
+    return `(call $${e.name} ${generateStatementsList(
+      e.stackFrameSetup,
+    )}) ${generateStatementsList(e.stackFrameTearDown)}`;
+  } else if (expr.type === "Const") {
+    const e = expr as WasmConst;
+    return `(${e.variableType}.const ${e.value.toString()})`;
+  } else if (expr.type === "LocalGet") {
+    const e = expr as WasmLocalGet;
+    return `(local.get $${e.name}${getPreStatementsStr(e.preStatements)})`;
+  } else if (expr.type === "GlobalGet") {
+    const e = expr as WasmGlobalGet;
+    return `(global.get $${e.name}${getPreStatementsStr(e.preStatements)})`;
+  } else if (
+    expr.type === "ArithmeticExpression" ||
+    expr.type === "ComparisonExpression"
+  ) {
+    const e = expr as WasmArithmeticExpression | WasmComparisonExpression;
+    //TODO: support different op types other than i32
+    return `(${getBinaryInstruction(e.operator)} ${generateExprStr(
+      e.leftExpr,
+    )} ${generateExprStr(e.rightExpr)})`;
+  } else if (expr.type === "LocalSet" || expr.type === "GlobalSet") {
+    return generateStatementStr(expr);
+  } else if (expr.type === "BooleanExpression") {
+    const e = expr as WasmBooleanExpression;
+    // TODO: need to know type of the variable to set the correct instruction
+    if (e.isNegated) {
+      return `(i32.eqz ${generateExprStr(e.expr)})`;
+    } else {
+      return `(i32.ne (i32.const 0) ${generateExprStr(e.expr)})`;
+    }
+  } else if (expr.type === "AndExpression") {
+    const e = expr as WasmAndExpression;
+    return `(i32.and ${generateExprStr(e.leftExpr)} ${generateExprStr(
+      e.rightExpr,
+    )})`;
+  } else if (expr.type === "OrExpression") {
+    const e = expr as WasmOrExpression;
+    return `(i32.or ${generateExprStr(e.leftExpr)} ${generateExprStr(
+      e.rightExpr,
+    )})`;
+  } else if (expr.type === "MemorySize") {
+    return "(memory.size)";
+  } else if (expr.type === "MemoryLoad") {
+    const n = expr as WasmMemoryLoad;
+    return `(${getWasmMemoryLoadInstruction(
+      n.varType,
+      n.numOfBytes,
+    )}${getPreStatementsStr(n.preStatements)} ${generateExprStr(expr.addr)})`;
+  } else if (expr.type === "MemoryStore") {
+    return generateStatementStr(expr);
+  } else {
+    console.assert(
+      false,
+      `WAT GENERATOR ERROR: Unhandled case during WAT node to string conversion\n${JSON.stringify(
+        expr,
+      )}`,
+    );
+  }
+}
---
+++ ctowasm/src/wat-generator/util.ts
@@ -0,0 +1,125 @@
+/**
+ * Utility functions for WAT generation.
+ */
+
+import { BinaryOperator, ComparisonOperator } from "~src/common/constants";
+import { WasmExpression, WasmStatement } from "~src/wasm-ast/core";
+import { WasmType } from "~src/wasm-ast/types";
+import { generateExprStr } from "~src/wat-generator/expression";
+import { generateStatementStr } from "~src/wat-generator/statement";
+
+/**
+ * Function that returns a line in wat file with given level of identation & ending with newline.
+ */
+export function generateLine(line: string, indentation: number) {
+  return "\t".repeat(indentation) + line + "\n";
+}
+
+/**
+ * Function that takes a block of strings (newline separated lines) and indents them with given indentation.
+ */
+export function generateBlock(block: string, indentation: number) {
+  let watStr = "";
+  for (const line of block.split("\n")) {
+    watStr += generateLine(line, indentation);
+  }
+  return watStr;
+}
+
+/**
+ * Returns the appropriate memory instruction for a memory load.
+ * TODO: support unsigned types in future.
+ */
+export function getWasmMemoryLoadInstruction(
+  varType: WasmType,
+  numOfBytes: number,
+) {
+  if (
+    ((varType === "i32" || varType === "f32") && numOfBytes === 4) ||
+    ((varType === "i64" || varType === "f64") && numOfBytes === 8)
+  ) {
+    return `${varType}.load`;
+  }
+  return `${varType}.load${numOfBytes.toString()}_s`;
+}
+
+export function getWasmMemoryStoreInstruction(
+  varType: WasmType,
+  numOfBytes: number,
+) {
+  if (
+    ((varType === "i32" || varType === "f32") && numOfBytes === 4) ||
+    ((varType === "i64" || varType === "f64") && numOfBytes === 8)
+  ) {
+    return `${varType}.store`;
+  }
+  return `${varType}.store${numOfBytes.toString()}`;
+}
+
+/**
+ * Returns string of argument expressions that are provided to function calls, or certain instructions like add.
+ * Basically any instruction that needs to read multiple variables from the stack can use this function to conveniently attach all
+ * the subexpressions that form the stack values.
+ */
+export function generateArgString(exprs: WasmExpression[]) {
+  let argsStr = "";
+  for (const arg of exprs) {
+    argsStr += generateExprStr(arg) + " ";
+  }
+  return argsStr.trim();
+}
+
+/**
+ * Given an array of WASM statement AST nodes, returns a list of WAT statements.
+ */
+export function generateStatementsList(
+  statements: (WasmStatement | WasmExpression)[],
+) {
+  return statements
+    .map((s) => generateStatementStr(s) ?? generateExprStr(s as WasmExpression))
+    .join(" ");
+}
+
+/**
+ * Returns the correct WAT binary instruction, given a binary operator.
+ * TODO: add support for other types and unsigned/signed ints.
+ */
+export function getBinaryInstruction(
+  operator: BinaryOperator | ComparisonOperator,
+) {
+  switch (operator) {
+    case "+":
+      return "i32.add";
+    case "-":
+      return "i32.sub";
+    case "*":
+      return "i32.mul";
+    case "/":
+      return "i32.div_s";
+    case "%":
+      return "i32.rem_s";
+    case "<":
+      return "i32.lt_s";
+    case "<=":
+      return "i32.le_s";
+    case "!=":
+      return "i32.ne";
+    case "==":
+      return "i32.eq";
+    case ">=":
+      return "i32.ge_s";
+    case ">":
+      return "i32.gt_s";
+  }
+}
+
+export function getPreStatementsStr(
+  preStatements?: (WasmStatement | WasmExpression)[],
+) {
+  const s = preStatements
+    ? preStatements.map(
+        (s) => generateStatementStr(s) ?? generateExprStr(s as WasmExpression),
+      )
+    : [];
+  return s.length > 0 ? " " + s.join(" ") : "";
+}
