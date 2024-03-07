a76bd126b20215c1cedb5f0ba869e38d6fa1f2f6
benmurphyy <47269122+benmurphyy@users.noreply.github.com>
2024-03-07 16:35:58 +0800

    Modify constants

--- ctowasm/src/common/constants.ts
+++ ctowasm/src/common/constants.ts
@@ -7,5 +7,5 @@ export const WASM_ADDR_SIZE = 4; // number of bytes of a wasm address
 export const POINTER_SIZE = WASM_ADDR_SIZE; // size of a pointer in bytes - should be same as WASM_ADDR_SIZE
 export const SIZE_T = "unsigned int"; // implmentation-defined
 export const PTRDIFF_T = "signed int"; // defined type for difference between pointers
-export const POINTER_TYPE = "unsigned int"; // type equivalent to pointer TODO: check this
+export const POINTER_TYPE = "unsigned int"; // type equivalent to pointer for this compiler implementation
 export const ENUM_DATA_TYPE = "signed int"; // the datatype that enum directly corresponds to in this compiler implementation
