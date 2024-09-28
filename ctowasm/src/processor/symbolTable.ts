c4ecf879fdf5652149bb862b8ca3f32b54be0bc2
Mohammad.Sh <mohammad.shahabadifrh@gmail.com>
2024-09-28 17:20:51 +0800

    feat: change null space allocated value to something descriptive

--- ctowasm/src/processor/symbolTable.ts
+++ ctowasm/src/processor/symbolTable.ts
@@ -73,7 +73,9 @@ export class SymbolTable {
     } else {
       this.externalFunctions = {};
       this.parentTable = null;
-      this.dataSegmentByteStr = { value: '\\00\\00\\00\\00'  };
+      // Initial value to reserve for the null space
+      this.dataSegmentByteStr = { value: '\\d0\\e0\\b0\\f0'  };
+      // 4 Bytes are reserved for the null space
       this.dataSegmentOffset = { value: 4 };
       this.functionTable = [];
       this.functionTableIndexes = {};
